2575dbf95f846582809a427e321df28c773d8e5f
Brian Brenner <bbrenner@nerdwallet.com>
2023-12-01 12:04:00 -0500

    changing to set

--- 01/part1.ts
+++ 01/part1.ts
@@ -4,14 +4,14 @@ function main() {
   const data = fs.readFileSync('input.txt', 'utf-8');
   const dataArr: string[] = data.split('\n');
 
-  const nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  const nums: Set<string> = new Set(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']);
   let sum = 0;
   for (const row of dataArr) {
-    const first = row.split('').find((x: string) => nums.includes(x))!;
+    const first = row.split('').find((x: string) => nums.has(x))!;
     const last = row
       .split('')
       .reverse()
-      .find((x: string) => nums.includes(x))!;
+      .find((x: string) => nums.has(x))!;
     sum += Number(first + last);
   }
 
